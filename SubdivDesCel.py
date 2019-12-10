bcca41c128a46bab6895a1c1187c814c64cee422
LymphoidComa <Joseph.wedgewoo@gmail.com>
2019-12-10 13:27:46 +0100

    Create SubdivDesCel.py
    Subdiv des cellules et placement de cubes, la hauterr et largeur des cubes est fixe mais ca serait bien de la lier aux parametres du labyrinthe

---
+++ SubdivDesCel.py
@@ -0,0 +1,63 @@
+import maya.cmds as cmds
+import random
+HauteurDeMaison = 0.5
+LongeurDeMaison = 0.2
+LargeurDeMaison =0.2
+
+
+ListeDsMurs =cmds.ls("Mur*")
+
+#print(ListeDsMurs)
+
+                    ##fonction qui definit la maison
+
+def Maison():
+    
+    a = cmds.polyCube(w=LargeurDeMaison,h=HauteurDeMaison+random.uniform(0, 0.3),d=LongeurDeMaison,n=Maison)
+    cmds.rename('Maison#')
+    
+                    ##Pour chaque case subdivise2fois 
+    
+for j in range(len(ListeDsMurs)):
+    
+    cmds.select(ListeDsMurs[j])
+    
+    list = cmds.ls(sl=True,fl = True)
+
+    
+    cmds.polySubdivideFacet(dvv=2)
+
+                   ##deselectionne les vertices du bords et fait une liste de points
+    #print(list)
+    for item in list:
+        vtxCount = cmds.polyEvaluate(v=True)
+        #print(vtxCount)
+        cmds.select(cl=True)
+        cmds.select(item+'.vtx[0:'+str(vtxCount)+']', add=True)
+        #cmds.polySelectConstraint(pp=6)
+        mel.eval('PolySelectTraverse 2')
+        pointsInternes = cmds.ls(selection = True , sn=True, fl = True)
+        
+                   ##Bouge chaque point selon un random et place une instance de maison
+
+    #print(pointsInternes)
+
+    for i in range(len(pointsInternes)):
+        
+        if len(pointsInternes) > 1:
+
+    
+            maSel = cmds.select(pointsInternes[i])
+            cmds.move(random.uniform(0, 0.2) ,0,random.uniform(0, 0.2) ,r=True)
+            posDesObjs = cmds.xform(maSel,q=True,ws=True, t=True)
+            NvlMaison = Maison()
+            #cmds.move(posDesObjs[0],posDesObjs[1],posDesObjs[2],'Maison'+str(i+1))    
+            cmds.move(posDesObjs[0],posDesObjs[1]+HauteurDeMaison/2.0,posDesObjs[2])
+
+#cmds.move
+
+#materialName = 'sol'#nom du matos dans l'hypershade
+#shadingGroup = cmds.listConnections(materialName, type='shadingEngine')
+#print(shadingGroup)
+#componentsWithMaterial = cmds.sets(shadingGroup, q=True)
+#print(componentsWithMaterial)
